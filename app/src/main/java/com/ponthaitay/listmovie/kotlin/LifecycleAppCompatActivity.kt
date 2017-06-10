3304faf5a52271a62532fa49056b33f9ab8981a8
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-06-10 22:37:13 +0700

    remove newline.

--- app/src/main/java/com/ponthaitay/listmovie/kotlin/LifecycleAppCompatActivity.kt
+++ app/src/main/java/com/ponthaitay/listmovie/kotlin/LifecycleAppCompatActivity.kt
@@ -5,7 +5,6 @@ import android.arch.lifecycle.LifecycleRegistryOwner
 import android.support.v7.app.AppCompatActivity
 
 open class LifecycleAppCompatActivity : AppCompatActivity(), LifecycleRegistryOwner {
-    
     @Suppress("LeakingThis")
     private val mRegister = LifecycleRegistry(this)
 
