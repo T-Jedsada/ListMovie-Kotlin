d54aa3a1fb54a77e9de61f66cd853ffc8ad22091
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-06-10 23:25:55 +0700

    improvment code.

--- app/src/test/java/com/ponthaitay/listmovie/kotlin/movie/MovieModelTest.kt
+++ app/src/test/java/com/ponthaitay/listmovie/kotlin/movie/MovieModelTest.kt
@@ -1,5 +1,7 @@
 package com.ponthaitay.listmovie.kotlin.movie
 
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
 import com.ponthaitay.listmovie.kotlin.JsonMockUtility
 import com.ponthaitay.listmovie.kotlin.RxSchedulersOverrideRule
 import com.ponthaitay.listmovie.kotlin.api.MovieApi
@@ -8,46 +10,36 @@ import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Matchers.anyInt
-import org.mockito.Matchers.anyString
-import org.mockito.Mock
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.ArgumentMatchers.anyString
 import org.mockito.MockitoAnnotations
-import org.mockito.runners.MockitoJUnitRunner
-import org.powermock.api.mockito.PowerMockito.`when`
 import retrofit2.Response
 
-@RunWith(MockitoJUnitRunner::class)
 class MovieModelTest {
 
-    @Rule @JvmField var rxSchedulerRule = RxSchedulersOverrideRule()
+    @Rule @JvmField val rxSchedulerRule = RxSchedulersOverrideRule()
 
-    @Mock var mockApi: MovieApi? = null
+    private var mockApi: MovieApi = mock()
 
-    private var mainViewModel: MainViewModel? = null
-    private var movieModel: MovieModel? = null
-    private var jsonUtil: JsonMockUtility? = null
+    private var mainViewModel = MainViewModel()
+    private var movieModel = MovieModel()
+    private var jsonUtil = JsonMockUtility()
 
     @Before
     fun setUp() {
-        jsonUtil = JsonMockUtility()
         MockitoAnnotations.initMocks(this)
-
-        movieModel = MovieModel()
-        movieModel?.setApi(mockApi!!)
-
-        mainViewModel = MainViewModel()
-        mainViewModel?.setMovieModel(movieModel!!)
+        movieModel.setApi(mockApi)
+        mainViewModel.setMovieModel(movieModel)
     }
 
     @Test
     fun requestMovieSuccess() {
-        val mockResult = jsonUtil!!.getJsonToMock("movie_success.json", MovieDao::class.java)
+        val mockResult = jsonUtil.getJsonToMock("movie_success.json", MovieDao::class.java)
         assertEquals(mockResult.result.size, 20)
         val mockResponse = Response.success(mockResult)
         val mockObservable = Observable.just(mockResponse)
-        `when`(movieModel!!.observableMovie(anyString(), anyString(), anyInt())).thenReturn(mockObservable)
-        mainViewModel!!.getListMovie("api_key", "sort_by")
+        whenever(movieModel.observableMovie(anyString(), anyString(), anyInt())).thenReturn(mockObservable)
+        mainViewModel.getListMovie("api_key", "sort_by")
         val testObserver = mockObservable.test()
         testObserver.awaitTerminalEvent()
         testObserver.assertNoErrors()
@@ -59,8 +51,8 @@ class MovieModelTest {
     fun requestMovieError() {
         val throwable = Throwable("error")
         val mockObservable = Observable.error<Response<MovieDao>>(throwable)
-        `when`(movieModel!!.observableMovie(anyString(), anyString(), anyInt())).thenReturn(mockObservable)
-        mainViewModel?.getListMovie("api_key", "sort_by")
+        whenever(movieModel.observableMovie(anyString(), anyString(), anyInt())).thenReturn(mockObservable)
+        mainViewModel.getListMovie("api_key", "sort_by")
         val testObserver = mockObservable.test()
         testObserver.awaitTerminalEvent()
         testObserver.assertNotComplete()
@@ -69,12 +61,12 @@ class MovieModelTest {
 
     @Test
     fun requestMovieEmpty() {
-        val mockResult = jsonUtil!!.getJsonToMock("movie_empty.json", MovieDao::class.java)
+        val mockResult = jsonUtil.getJsonToMock("movie_empty.json", MovieDao::class.java)
         assertEquals(mockResult.result.size, 0)
         val mockResponse = Response.success(mockResult)
         val mockObservable = Observable.just(mockResponse)
-        `when`(movieModel!!.observableMovie(anyString(), anyString(), anyInt())).thenReturn(mockObservable)
-        mainViewModel?.getListMovie("api_key", "sort_by")
+        whenever(movieModel.observableMovie(anyString(), anyString(), anyInt())).thenReturn(mockObservable)
+        mainViewModel.getListMovie("api_key", "sort_by")
         val testObserver = mockObservable.test()
         testObserver.awaitTerminalEvent()
         testObserver.assertNoErrors()
@@ -84,11 +76,11 @@ class MovieModelTest {
 
     @Test
     fun requestMovieInvalidAPI() {
-        val mockResult = jsonUtil!!.getJsonToMock("movie_invalid_api.json", MovieDao::class.java)
+        val mockResult = jsonUtil.getJsonToMock("movie_invalid_api.json", MovieDao::class.java)
         val mockResponse = Response.success(mockResult)
         val mockObservable = Observable.just(mockResponse)
-        `when`(movieModel!!.observableMovie(anyString(), anyString(), anyInt())).thenReturn(mockObservable)
-        mainViewModel?.getListMovie("api_key", "sort_by")
+        whenever(movieModel.observableMovie(anyString(), anyString(), anyInt())).thenReturn(mockObservable)
+        mainViewModel.getListMovie("api_key", "sort_by")
         val testObserver = mockObservable.test()
         testObserver.awaitTerminalEvent()
         testObserver.assertNoErrors()
